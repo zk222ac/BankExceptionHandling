26ce01618dc964b6d3668c8eb625eb2faa848d4a
Zuhair Haroon Khan <zk222ac@gmail.com>
2020-12-07 22:48:10 +0100

    Add project files.

---
+++ BankExceptionHandling/Account.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankExceptionHandling
+{
+   public class Account
+    {
+        private string _name;
+        private decimal _balance;
+
+        public Account()
+        {
+                
+        }
+        public Account(string name, decimal balance)
+        {
+            _name = name;
+            if (balance <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(_balance), _balance, "Balance should be greater than Zero");
+            }
+            _balance = balance;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set 
+            { 
+                _name = value;
+            }
+        }
+        public decimal Balance
+        {
+            get { return _balance; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(_balance), _balance, "Balance should be greater than Zero");
+                }
+                _balance = value;
+            }
+        }
+
+        // 
+        public void Deposit(decimal amount)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), amount, "Amount should be positive");
+            }
+            _balance = _balance + amount;
+        }
+
+        // Balance should be greater than withdrawal amount
+        public void WithDraw(decimal amount)
+        {
+            if(_balance < amount)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), amount, "Balance should be greater than Amount");
+
+            }
+            _balance = _balance - amount;
+        }
+    }
+}
---
+++ BankExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BankExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Account acc = new Account("zuhair", 10000);
+                Account acc1 = new Account() { Name = "David", Balance = 2000.00M };
+                acc.Deposit(234);
+                Console.WriteLine(acc.Balance);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
